28d718d47600a330c4b7c21ccc08f11420ed03db
toby941 <toby941@gmail.com>
2011-04-19 00:55:29 +0800

    完善mobile代码

--- m/index.php
+++ m/index.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html> 
 <html> 
   <head> 
+  	<meta http-equiv="Content-type" content="text/html; charset=UTF-8">
   <title>Page Title</title> 
 <link rel="stylesheet" href="http://code.jquery.com/mobile/1.0a4.1/jquery.mobile-1.0a4.1.min.css">
   <script type="text/javascript" src="http://code.jquery.com/jquery-1.5.min.js"></script>
@@ -15,6 +16,15 @@
   </div>
 
   <div data-role="content"> 
+  <ul data-role="listview" data-dividertheme="e">
+  <li data-role="list-divider">Seamless List (margin-less)</li>
+<li><a href="#men_shirts" data-transition="slide">
+<h3>衬衫</h3>
+<p>长短款男士衬衫，3折起卖</p>
+</a></li>
+<li><a href="#bar" data-transition="slide">Internal Link 2</a></li>
+<li><a href="#" data-transition="slide">Example Item 3</a></li>
+</ul>
     <p><a href="#about">About this app</a></p>    
   </div>
 
@@ -28,6 +38,40 @@
 
   <div data-role="content"> 
     <p>This app rocks! <a href="#home">Go home</a></p>    
+  </div>
+
+</div>
+<div data-role="page" id="men_shirts">
+
+  <div data-role="header">
+    <h1>Men's Shirts</h1>
+  </div>
+
+  <div data-role="content"> 
+  <a href="#210204023035"> <img src="/images/man/20110412/s/210204023035_m1.jpg" />  </a>
+   <a href="#210204023035"> <img src="/images/man/20110412/s/210204023035_m2.jpg" />  </a>
+    <a href="#210204023035"> <img src="/images/man/20110412/s/210204023035_m1.jpg" />  </a>
+     <a href="#210204023035"> <img src="/images/man/20110412/s/210204023035_m2.jpg" />  </a>
+      <a href="#210204023035"> <img src="/images/man/20110412/s/210204023035_m1.jpg" />  </a>
+       <a href="#210204023035"> <img src="/images/man/20110412/s/210204023035_m1.jpg" />  </a>
+        <a href="#210204023035"> <img src="/images/man/20110412/s/210204100095_m5.jpg" />  </a>
+         <a href="#210204023035"> <img src="/images/man/20110412/s/210204023035_m1.jpg" />  </a>
+          <a href="#210204023035"> <img src="/images/man/20110412/s/210204100095_x2.jpg" />  </a>
+           <a href="#210204023035"> <img src="/images/man/20110412/s/210204023035_m2.jpg" />  </a>
+            <a href="#210204023035"> <img src="/images/man/20110412/s/210204023035_m1.jpg" />  </a>
+             <a href="#210204023035"> <img src="/images/man/20110412/s/210204049037_m4.jpg" />  </a>
+              <a href="#210204023035"> <img src="/images/man/20110412/s/210204023035_m1.jpg" />  </a>
+               <a href="#210204023035" data-transition="pop"> <img src="/images/man/20110412/s/210204100095_m1.jpg" />  </a>
+   
+  </div>
+
+</div>
+<div data-role="page" id="210204023035">
+<div data-role="header">
+    <h1>About This App</h1>
+  </div>
+ <div data-role="content" > 
+    <img src="/images/man/20110412/n/210204100095_m1.jpg" /> 
   </div>
 
 </div>
